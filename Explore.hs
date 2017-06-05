bbff023348dd047a808e336fc01da5263dda37f7
Maximilian Bosch <maximilian@mbosch.me>
2017-06-05 21:16:36 +0200

    Finalize POC of GitHub API client
    resolves #2

--- Explore.hs
+++ Explore.hs
@@ -5,10 +5,11 @@ import qualified GitHub.Endpoints.Search as GitHub
 import qualified GitHub.Data as GitHub
 
 import Data.Text (Text, pack)
+import Data.List as L
 import Data.Vector as V
 
 load q = do
-  repos <- GitHub.searchRepos $ "q=" `mappend` q
+  repos <- GitHub.searchRepos q
   return $ case repos of
     Left e -> pack $ show e
     Right r -> let
@@ -17,8 +18,10 @@ load q = do
                in
                  case n of
                    0 -> ""
-                   _ -> resolve $ GitHub.repoDescription $ V.head v
+                   _ -> pack $ show $
+                        L.intercalate "\n" $
+                        V.toList $
+                        V.map (\x -> resolve $ GitHub.repoDescription x) v
 
--- TODO just a shortcut for a string-based getOrElse. Needs refactoring later...
-resolve :: Maybe Text -> Text
-resolve s = pack $ maybe "" show s
+resolve :: Maybe Text -> [Char]
+resolve s = maybe "" show s
