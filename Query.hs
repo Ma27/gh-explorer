c80c3f1a0b5aa171bc95b31bbcdc075756f653df
Maximilian Bosch <maximilian@mbosch.me>
2017-07-21 11:40:37 +0200

    Implement simple behavior to match `topic` search requests

--- Query.hs
+++ Query.hs
@@ -6,6 +6,8 @@ import qualified Data.Text as T
 import Data.Aeson.Types
 
 import GHC.Generics
+import Data.Maybe
+import Text.Regex
 
 data QueryComponent = QueryComponent { name :: T.Text
                                      , content :: [T.Text]
@@ -14,4 +16,14 @@ data QueryComponent = QueryComponent { name :: T.Text
 instance ToJSON QueryComponent
 
 parseQ :: String -> [QueryComponent]
-parseQ q = [QueryComponent "default" $ T.splitOn " " $ T.pack q]
+parseQ q = resolve $ match q
+  where
+    resolve :: Maybe (String, String, String, [String]) -> [QueryComponent]
+    resolve Nothing = [] :: [QueryComponent]
+    resolve (Just (_, _, _, l)) = map toCmp $ filter (/= "") l
+
+    toCmp :: String -> QueryComponent
+    toCmp s = QueryComponent "topic" $ T.splitOn " " $ T.pack s
+
+    match :: String -> Maybe (String, String, String, [String])
+    match = matchRegexAll (mkRegex "topic:([a-z]+)|\"([a-z ]+)\"")
